3b4be7faafa8f4052930606655002cf321445552
jllamas@insecami.com.mx <jllamas@insecami.com>
2021-11-24 12:53:25 -0600

    First Commit

---
+++ sitetest/settings/dev.py
@@ -0,0 +1,27 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-o16)qpy%xvq9k=lvr+$$372rasb^!ww40h+70hzb9jkain3v7j'
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ['*'] 
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+INSTALLED_APPS = INSTALLED_APPS + [
+    'debug_toolbar'
+]
+
+MIDDLEWARE = MIDDLEWARE + [
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
+]
+
+INTERNAL_IPS = ("127.0.0.1", "172.17.0.1")
+
+try:
+    from .local import *
+except ImportError:
+    pass
